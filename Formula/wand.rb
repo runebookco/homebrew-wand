bba56f309c20842d0e32beccd66924290a518aec
Matte Noble <me@mattenoble.com>
2024-04-23 18:17:59 -0700

    Initial wand formula

---
+++ Formula/wand.rb
@@ -0,0 +1,15 @@
+class Wand < Formula
+  version "0.0.1"
+  desc "Runebook CLI"
+  homepage "https://runebook.co"
+
+  if OS.mac?
+    url "https://github.com/runebookco/wand/releases/download/#{version}/wand-#{version}-x86_64-apple-darwin.tar.gz"
+  elsif OS.linux?
+    url "https://github.com/runebookco/wand/releases/download/#{version}/wand-#{version}-x86_64-unknown-linux-gnu.tar.gz"
+  end
+
+  def install
+    bin.install "wand"
+  end
+end
---
+++ README.md
@@ -0,0 +1,10 @@
+# Runebook Wand Tap
+
+Homebrew tap to install `wand` – the Runebook CLI.
+
+## Usage
+
+```
+brew tap runebookco/wand
+brew install wand
+```
